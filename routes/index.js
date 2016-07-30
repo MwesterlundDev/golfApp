59b621e4b37174d39a041b223cfd8299764ecb2b
Mark Westerlund <mark.westerlund.dev@gmail.com>
2016-07-29 23:35:53 -0400

    Added json data, stubs for future DB

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
+var course = require("./data/course");
+var users = require("./data/users");
+var outing = require("./data/outing");
+
 router.route('/').get(function(req, res, next) {
   res.send('Hello World');
+
+
 });
+
+
+
+// GET course by name
+router.route('/course/:name').get(function(req, res, next) {
+  
+  var name = req.params.name;
+
+  //stub course  
+  res.send(course);
+
+  
+});
+
+// GET user by name
+router.route('/user/:name').get(function(req, res, next) {
+  
+  var name = req.params.name;
+
+  //stub send all users  
+  res.send(users);
+
+  
+});
+
+// GET outing by ID
+router.route('/outing/:id').get(function(req, res, next) {
+  
+  var id = req.params.id;
+
+  //stub outing 
+  res.send(outing);
+
+  
+});
+
 
 module.exports = router;
